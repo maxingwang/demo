6548a9500cb0ccd2d024378ecdfd766a1e42e200
viveduric_xwma <xwma@viveduric.com>
2019-12-05 16:40:41 +0800

    branch2

---
+++ hello.java
@@ -0,0 +1,6 @@
+if (isNotEmpty(devicesInstallAppCompletedMap.get(classroomId))) {
+            
+            if (isNotEmpty(devicesInstallAppCompletedMap.get(classroomId).get(packageName))) {
+               32423424
+            }
+        }
